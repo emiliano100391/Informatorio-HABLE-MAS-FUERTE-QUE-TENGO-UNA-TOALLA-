ebc00b1c98fa8db8b67f0484f2b4a8fec17bc802
emiliano100391 <emiliano100391@gmail.com>
2023-12-08 23:46:32 +0000

    frontend

--- blog-base/blog/settings/base.py
+++ blog-base/blog/settings/base.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'apps.usuarios',
     'apps.noticias',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
